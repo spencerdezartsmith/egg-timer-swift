f358626aad13d49080a8b23729b419a5ca71d0f4
Spencer Dezart-Smith <spencerdezartsmith@gmail.com>
2020-08-21 10:51:08 +1000

    Add egg boiling logic

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-
+    
+    @IBOutlet var label: UILabel!
+    @IBOutlet var progressBar: UIProgressView!
+    
+    let eggTimes: [String:Float] = ["Soft": 15.0, "Medium": 17.0, "Hard": 12.0]
+    
+    var secondsPassed : Float = 0.0
+    var timer = Timer()
+    var selectedBoilTime : Float = 0.0
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        let hardness = sender.currentTitle!
+        selectedBoilTime = eggTimes[hardness]!
+        timer.invalidate()
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        if secondsPassed < selectedBoilTime {
+            self.label.text = "Boiling..."
+            secondsPassed += 1
+            
+            progressBar.progress = secondsPassed / selectedBoilTime
+        } else {
+            timer.invalidate()
+            progressBar.progress = 0.0
+            self.label.text = "Egg is ready!"
+        }
+    }
+    
+    
 }
